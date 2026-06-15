a0b19d4e96922d01ec4a91a5d97a731550a2e554
Gabriel Franken <franken.gabriel2010@gmail.com>
2026-06-15 20:15:58 -0300

    Q5

--- Program.cs
+++ Program.cs
@@ -1,25 +1,23 @@
 ﻿using System;
-using System.Globalization;
 
 class Program
 {
     static void Main()
     {
-        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        Console.Write("Informe a quantidade de tarefas concluídas: ");
+        int tarefas = int.Parse(Console.ReadLine());
+        int meta = 50;
 
-        Console.Write("Informe o valor total da compra: ");
-        double valorOriginal = double.Parse(Console.ReadLine());
-
-        double desconto = 0;
-        if (valorOriginal >= 200.00)
+        if (tarefas >= meta)
         {
-            desconto = valorOriginal * 0.10;
+            Console.WriteLine("Meta atingida.");
+            Console.WriteLine($"Tarefas concluídas: {tarefas}");
         }
-
-        double valorFinal = valorOriginal - desconto;
-
-        Console.WriteLine($"Valor original: R$ {valorOriginal:F2}");
-        Console.WriteLine($"Desconto aplicado: R$ {desconto:F2}");
-        Console.WriteLine($"Valor final: R$ {valorFinal:F2}");
+        else
+        {
+            Console.WriteLine("Meta não atingida.");
+            int faltaram = meta - tarefas;
+            Console.WriteLine($"Faltaram {faltaram} tarefas.");
+        }
     }
 }
