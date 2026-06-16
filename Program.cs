71f4d2f2bd58dc34a9cbb1599c451e9f3e22470f
Gabriel Franken <franken.gabriel2010@gmail.com>
2026-06-15 21:04:14 -0300

    Q14

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Globalization;
 
 class Program
 {
     static void Main()
     {
-        Console.Write("Informe o número da tabuada: ");
-        int numero = int.Parse(Console.ReadLine());
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 
-        for (int i = 1; i <= 10; i++)
+        string[] ordinais = { "primeiro", "segundo", "terceiro", "quarto", "quinto" };
+        double soma = 0;
+
+        for (int i = 0; i < 5; i++)
         {
-            Console.WriteLine($"{numero} x {i} = {numero * i}");
+            Console.Write($"Informe o {ordinais[i]} valor: ");
+            soma += double.Parse(Console.ReadLine());
         }
+
+        double media = soma / 5;
+
+        Console.WriteLine($"Soma dos valores: {soma}");
+        Console.WriteLine($"Média dos valores: {media}");
     }
 }
