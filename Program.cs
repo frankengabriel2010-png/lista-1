a82c1eded76c6a1a762c7a423a38da2892ea304f
Gabriel Franken <franken.gabriel2010@gmail.com>
2026-06-15 21:00:18 -0300

    Q11

--- Program.cs
+++ Program.cs
@@ -1,34 +1,15 @@
 ﻿using System;
-using System.Globalization;
 
 class Program
 {
     static void Main()
     {
-        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        Console.Write("Informe o número final da contagem: ");
+        int numeroFinal = int.Parse(Console.ReadLine());
 
-        Console.Write("Informe da idade do passageiro: ");
-        int idade = int.Parse(Console.ReadLine());
-
-        Console.Write("Informe o valor normal da passagem: ");
-        double valorNormal = double.Parse(Console.ReadLine());
-
-        double valorPagar;
-
-        if (idade <= 5)
+        for (int i = 1; i <= numeroFinal; i++)
         {
-            valorPagar = 0;
+            Console.WriteLine(i);
         }
-        else if (idade >= 60)
-        {
-            valorPagar = valorNormal * 0.50;
-        }
-        else
-        {
-            valorPagar = valorNormal;
-        }
-
-        Console.WriteLine($"Valor normal da passagem: R$ {valorNormal:F2}");
-        Console.WriteLine($"Valor a pagar: R$ {valorPagar:F2}");
     }
 }
