34bb9147b8fa0e4e18028359e23955896aaacc56
Gabriel Franken <franken.gabriel2010@gmail.com>
2026-06-15 21:07:12 -0300

    Q16

--- Program.cs
+++ Program.cs
@@ -4,31 +4,21 @@ class Program
 {
     static void Main()
     {
-        int positivos = 0;
-        int negativos = 0;
-        int zeros = 0;
+        int senhaCorreta = 1234;
+        int senhaDigitada;
 
-        for (int i = 0; i < 10; i++)
+        do
         {
-            Console.Write("Informe um número: ");
-            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Informe a senha: ");
+            senhaDigitada = int.Parse(Console.ReadLine());
 
-            if (numero > 0)
+            if (senhaDigitada != senhaCorreta)
             {
-                positivos++;
+                Console.WriteLine("Senha incorreta. Tente novamente.");
             }
-            else if (numero < 0)
-            {
-                negativos++;
-            }
-            else
-            {
-                zeros++;
-            }
-        }
 
-        Console.WriteLine($"Quantidade de positivos: {positivos}");
-        Console.WriteLine($"Quantidade de negativos: {negativos}");
-        Console.WriteLine($"Quantidade de zeros: {zeros}");
+        } while (senhaDigitada != senhaCorreta);
+
+        Console.WriteLine("Acesso permitido.");
     }
 }
